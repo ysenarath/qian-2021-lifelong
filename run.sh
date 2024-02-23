e1dbd766f5fcb1f3c26846821f1f6339a64ce199
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-02-22 22:53:38 -0500

    Add data files and update run.sh script

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 source ./venv/bin/activate
-python -m src.vae_lbsoinn
+python -m src.main --train_path ./data/processed/v1/train --dev_path ./data/processed/v1/valid --test_path ./data/processed/v1/test --cand_limit 1000 --keep_all_node --fix_per_task_mem_size
