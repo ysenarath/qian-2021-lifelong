c8cdb1a13fdc363e44e5ae8e7e9d7850ba1e5a60
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-02-23 19:20:15 -0500

    Add scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,2 +1,3 @@
+cd ..
 source ./venv/bin/activate
 python -m src.main --train_path ./data/processed/v1/train --dev_path ./data/processed/v1/valid --test_path ./data/processed/v1/test --cand_limit 1000 --keep_all_node --fix_per_task_mem_size
